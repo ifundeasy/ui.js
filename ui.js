5ad62a221d2634f0348556e297a05e5439c476f4
Afa Rappresent <mail.vudin@gmail.com>
2015-04-04 14:28:36 +0700

    adding reverse function

--- ui.js
+++ ui.js
@@ -8,8 +8,8 @@
  * Date : 3/30/15
  */
 
-(function(parent){
-	var UI = function UI () {
+(function (parent) {
+	var UI = function UI() {
 		this.obj = new Object();
 	};
 	UI.prototype.new = function (obj) {
@@ -43,7 +43,7 @@
 		var exception = ['css', 'text', 'style', 'html', 'el'];
 
 		if (obj.constructor === Object && (Object.keys(obj).length)) {
-			obj.id = obj.id || ('ui' + (Math.random()*10000).toString(32));
+			obj.id = obj.id || ('ui' + (Math.random() * 10000).toString(32));
 			Object.keys(obj).forEach(function (key, i) {
 				if (obj[key].constructor == String) {
 					if (exception.indexOf(key) === -1) {
@@ -127,9 +127,93 @@
 		}
 		return (me.element);
 	};
+	//reverse a Element (single) to Ui format
+	UI.prototype.reverse = function (element) {
+		function get(elem) {
+			var node = {};
 
-	UI.prototype.reverse = function () {
+			getEl(elem, node);
+			getClass(elem, node);
+			getAttr(elem, node);
+			getStyle(elem, node);
+			getText(elem, node);
+			getItems(elem, node);
 
+			return node;
+		}
+
+		function getEl(elem, node) {
+			node["el"] = elem.nodeName.toLowerCase();
+		}
+
+		function getClass(elem, node) {
+			node["class"] = elem.classList.toString();
+			if (!elem.classList.toString().length) delete node["class"];
+		}
+
+		function getAttr(elem, node) {
+			if (elem.attributes.length > 0) {
+				for (var i = 0; i < elem.attributes.length; i++) {
+					node[elem.attributes[i].nodeName] = elem.attributes[i].value
+				}
+			}
+		}
+
+		function getStyle(elem, node) {
+			if (elem.style.length) {
+				node['css'] = {};
+				for (var i = 0; i < elem.style.length; i++) {
+					var key = '';
+					var x = elem.style[i].split('-');
+					for (var j = 1; j < x.length; j++) {
+						x[j] = x[j].replace(x[j][0], x[j].substr(0, 1).toUpperCase());
+					}
+					x.forEach(function (i) {
+						key += i
+					});
+					node['css'][elem.style[i]] = elem.style[key]
+				}
+			}
+		}
+
+		function getText(elem, node) {
+			for (var k = 0; k < elem.childNodes.length; k++) {
+				var child = elem.childNodes[k];
+				if (elem.childNodes[k].nodeType == 3) {
+					if (
+						(child.nodeValue.replace(/\t|\n|\s/gi, '').length > 0) ||
+						(child.nodeName.toLowerCase() == 'pre')
+					) {
+						node["text"] = child.nodeValue;
+					}
+				}
+			}
+		}
+
+		function getItems(elem, node) {
+			node["items"] = '';
+			if (elem.childElementCount > 0) {
+				elem.childElementCount == 1 ? node["items"] = new Object() : node["items"] = new Array();
+			}
+			switch (node["items"].constructor) {
+				case Object :
+					node["items"] = get(elem.children[0], {});
+					break;
+				case Array :
+					for (var k = 0; k < elem.childElementCount; k++) {
+						node["items"].push(get(elem.children[k], {}));
+					}
+					break;
+				default :
+					delete node["items"];
+					break;
+			}
+		}
+
+		if (element instanceof Element) {
+			return get(element);
+		}
+		console.error('Make sure to assign a paramater instanceof Element');
 	};
 
 	//set global variable
